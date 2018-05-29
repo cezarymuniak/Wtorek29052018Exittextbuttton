330a83ea251add6a099e01a5f1ddb5cc500b6fc0
cezarymuniak <34242716+cezarymuniak@users.noreply.github.com>
2018-05-29 20:17:58 +0200

    Zrobione wszystko według https://developer.android.com/guide/topics/ui/layout/recyclerview#java
     plus dodana linijka w
    
     private String[] myDataset = {"hellow"}; w MainActivity

--- app/src/main/java/android/com/wtorek29052018exittextbuttton/MainActivity.java
+++ app/src/main/java/android/com/wtorek29052018exittextbuttton/MainActivity.java
@@ -4,11 +4,16 @@ import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
-
+    private RecyclerView mRecyclerView;
+    private RecyclerView.Adapter mAdapter;
+    private RecyclerView.LayoutManager mLayoutManager;
+    private String[] myDataset = {"hellow"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -24,6 +29,23 @@ public class MainActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        mRecyclerView = (RecyclerView) findViewById(R.id.my_recycler_view);
+
+//        // use this setting to improve performance if you know that changes
+//        // in content do not change the layout size of the RecyclerView
+//        mRecyclerView.setHasFixedSize(true);
+
+        // use a linear layout manager
+        mLayoutManager = new LinearLayoutManager(this);
+        mRecyclerView.setLayoutManager(mLayoutManager);
+
+
+        // specify an adapter (see also next example)
+        mAdapter = new MyAdapter(myDataset);
+        mRecyclerView.setAdapter(mAdapter);
+
+
     }
 
 
